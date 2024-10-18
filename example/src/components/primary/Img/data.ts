b82cab371301775358e6bb39445459cd1a3134e1
imagine10255 <imagine10255@gmail.com>
2024-10-18 09:32:58 +0800

    fix sample image

--- example/src/components/primary/Img/data.ts
+++ example/src/components/primary/Img/data.ts
@@ -1,7 +1,7 @@
 export const images = [
-    'https://acrool-react-picker.pages.dev/sample/1.jpg',
-    'https://acrool-react-picker.pages.dev/sample/2.jpg',
-    'https://acrool-react-picker.pages.dev/sample/3.jpg',
-    'https://acrool-react-picker.pages.dev/sample/4.jpg',
-    'https://acrool-react-picker.pages.dev/sample/5.jpg',
+    'https://acrool-react-img.pages.dev/static/sample/1.jpg',
+    'https://acrool-react-img.pages.dev/static/sample/2.jpg',
+    'https://acrool-react-img.pages.dev/static/sample/3.jpg',
+    'https://acrool-react-img.pages.dev/static/sample/4.jpg',
+    'https://acrool-react-img.pages.dev/static/sample/5.jpg',
 ];
